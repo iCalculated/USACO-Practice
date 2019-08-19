3906f29208ff9f84e4710c79efa9735524c82d90
iCalculated <fufa0001@gmail.com>
2019-08-19 05:44:37 -0500

    It worked for the first time! (editing while awake is cool)

--- combo.py
+++ combo.py
@@ -10,9 +10,9 @@ fout = open("combo.out","w")
 dials, *combos = fin.readlines()
 dials = int(dials)
 combos = [list(map(int,combo.split())) for combo in combos]
-first_diff = min(abs(combos[0][0] - combos[1][0]), abs(combos[0][0] + combos[1][0] - dials + 2)%dials)
-second_diff = min(abs(combos[0][1] - combos[1][1]), abs(combos[0][1] + combos[1][1] - dials + 2)%dials)
-third_diff = min(abs(combos[0][2] - combos[1][2]), abs(combos[0][2] + combos[1][2] - dials + 2)%dials)
+first_diff = min(abs(combos[0][0] - combos[1][0]), abs(dials-2*max(combos[0][0],combos[1][0])+combos[0][0]+combos[1][0]))
+second_diff = min(abs(combos[0][1] - combos[1][1]), abs(dials-2*max(combos[0][1],combos[1][1])+combos[0][1]+combos[1][1]))
+third_diff = min(abs(combos[0][2] - combos[1][2]), abs(dials-2*max(combos[0][2],combos[1][2])+combos[0][2]+combos[1][2]))
 subtract = 0
 if first_diff < 5 and second_diff < 5 and third_diff < 5:
     subtract = (5 - first_diff)*(5 - second_diff)*(5 - third_diff)
